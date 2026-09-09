f554918b3d1dad336acd8d6231b25646c09eac23
gerbimantap <muligarrendi2@gmail.com>
2026-09-09 15:08:08 +0700

    Update Supabase URL and anon key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Isi dengan Project URL dan anon/publishable key dari Supabase.
 // JANGAN memasukkan service_role key ke frontend.
-export const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY";
+export const SUPABASE_URL = "https://ekkwsbvvfvybreyclmhq.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_VyHcQVq513WaZOaPTuNoog_TUd4Zy2Y";
