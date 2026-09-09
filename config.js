c4a17c5e9208fbf38bdf31a3055b23493a4393f5
gerbimantap <muligarrendi2@gmail.com>
2026-09-09 18:56:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-// Isi dengan Project URL dan anon/publishable key dari Supabase.
-// JANGAN memasukkan service_role key ke frontend.
+// Konfigurasi Supabase SMPN 5 Gegerbitung
+// Jangan memasukkan service_role/secret key ke frontend.
+
 export const SUPABASE_URL = "https://ekkwsbvvfvybreyclmhq.supabase.co";
+
 export const SUPABASE_ANON_KEY = "sb_publishable_VyHcQVq513WaZOaPTuNoog_TUd4Zy2Y";
